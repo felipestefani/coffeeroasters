123eb043547e946aeada04134a5ebee2362479a4
Felipe Stefani <felipestefani@MacBook-Pro-de-Felipe.local>
2023-12-13 18:11:46 -0300

    add context variables to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,28 @@
 import style from "./style.module.css";
 import logo from "../../assets/shared/desktop/logo.svg";
+import hamburger from "../../assets/shared/mobile/icon-hamburger.svg";
+import closeIcon from "../../assets/shared/mobile/icon-close.svg";
 import Menu from "../Menu";
+import { useContext } from "react";
+import { MainContext } from "../../contexts/MainContext";
 
 const Header = () => {
+
+    const { isModalOpen, setIsModalOpen } = useContext(MainContext)
+
+
     return(
         <header className={style.header_container}>
             <nav className={style.navbar}>
-                <img src={logo} alt="logo" className={style.logo} />
-                <Menu />
+                <img src={logo} alt="logo" className={style.logo}/>
+                <Menu/>
+                {
+                    isModalOpen ? (
+                        <img src={closeIcon} alt="hamburger menu" className={style.hamburger_menu} onClick={() => setIsModalOpen(false)}/>
+                    ) : (
+                        <img src={hamburger} alt="hamburger menu" className={style.hamburger_menu} onClick={() => setIsModalOpen(true)}/>
+                    )
+                }
             </nav>
         </header>
     )
